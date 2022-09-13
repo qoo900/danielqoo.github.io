ce09ec2781680e85dc1c0bd2e2f4e9609931dba8
DANIEL KOO <tablern99@gmail.com>
2022-09-13 16:33:51 +0900

    Add files via upload

---
+++ static/js/3.f2cd96a8.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpfreepoli_Web=this.webpackJsonpfreepoli_Web||[]).push([[3],{75:function(e,i,s){"use strict";s.r(i),i.default=s.p+"static/media/we_need_a_designer.fd100bef.md"}}]);
+//# sourceMappingURL=3.f2cd96a8.chunk.js.map
