bf95d01a4c0471fc4ef7c7ed7187c9b4e203254e
unknown <tablern99@gmail.com>
2022-11-21 00:23:29 +0900

    updated_diary

--- js/code.js
+++ js/code.js
@@ -198,6 +198,66 @@ function getYear() {
 }
 
 
+function DdayPoetry() {
+	const countDownTimer = function (id, date) {
+		var _vDate = new Date(date); // 전달 받은 일자
+		var _second = 1000;
+		var _minute = _second * 60;
+		var _hour = _minute * 60;
+		var _day = _hour * 24;
+		var timer;
+
+		function showRemaining() {
+			var now = new Date();
+			var distDt = _vDate - now;
+
+			if (distDt < 0) {
+				clearInterval(timer);
+				document.getElementById(id).textContent = '어플이 출시 되었습니다!';
+				return;
+			}
+
+			var days = Math.floor(distDt / _day);
+			var hours = Math.floor((distDt % _day) / _hour);
+			var minutes = Math.floor((distDt % _hour) / _minute);
+			var seconds = Math.floor((distDt % _minute) / _second);
+
+			//document.getElementById(id).textContent = date.toLocaleString() + "까지 : ";
+			document.getElementById(id).textContent = days + '일 ';
+			document.getElementById(id).textContent += hours + '시간 ';
+			document.getElementById(id).textContent += minutes + '분 ';
+			document.getElementById(id).textContent += seconds + '초';
+		}
+
+		timer = setInterval(showRemaining, 1000);
+	}
+
+	var dateObj = new Date();
+	dateObj.setDate(dateObj.getDate() + 1);
+
+	//countDownTimer('Dday01', dateObj); // 내일까지
+	countDownTimer('Dday01', '11/30/2022 23:59'); // 2022년 11월 30일까지, 시간을 표시하려면 01:00 AM과 같은 형식을 사용한다. 
+	countDownTimer('Dday02', '12/31/2022 23:59'); // 2022년 12월 31일까지, 시간을 표시하려면 01:00 AM과 같은 형식을 사용한다.
+	//countDownTimer('Dday03', '04/01/2024'); // 2024년 4월 1일까지
+	//countDownTimer('Dday04', '04/01/2019'); // 2024년 4월 1일까지
+}
+
+function getAge(){
+	const today = new Date();
+	const birthDate = new Date(1989, 9, 19); // 생일 // 0 -> 1월, 1 -> 2월 ...
+
+	let age = today.getFullYear() - birthDate.getFullYear();
+	const m = today.getMonth() - birthDate.getMonth();
+	if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+		age--;
+	}
+
+	document.getElementById('getAge').innerHTML = age;
+	window.setTimeout(getAge, 1000);
+
+}
+
+
 ////////////////////////////////////////////////////
 
 
